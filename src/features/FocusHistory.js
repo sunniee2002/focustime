34e777897eec7eb2a613b71a234d9eae6ee6b5cb
sunniee2002 <sunniawaty.prayugo@gmail.com>
2022-03-24 17:33:41 +0700

    Initial Entry

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const FocusHistory = ({history}) => {
+  if (!history || !history.length) return <Text style={styles.title}>We haven't focused on anything yet. </Text>;
+
+  const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we focus on: </Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex:1,
+  },  
+  item: {
+    fontSizes: fontSizes.md,
+    color: colors.white,
+  },
+  title: {
+    color: colors.white,
+    fontSizes: fontSizes.md,
+    padding: spacing.md,
+    fontWeight: 'bold',
+  },
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  white: '#fff',
+  darkBlue: '#252250',
+  ProgBar: '#5E84E2',
+}
